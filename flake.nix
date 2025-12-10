b6838cc5ca2b38596331365bb6676f9a8aeb1942
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-12-10 15:23:30 +0900

    only stable release version

--- flake.nix
+++ flake.nix
@@ -1,93 +1,71 @@
 {
-description = "Zen Browser (auto-update flake)";
+  description = "Zen Browser Release (auto-update flake)";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-inputs = {
-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-# nvfetcher is referenced in the actions; not needed as an input here
-};
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
 
+    # generated.nix provides: zen.version / zen.src.url / zen.src.sha256
+    zenSrc = import ./generated.nix;
 
-outputs = { self, nixpkgs }: let
-system = "x86_64-linux";
-pkgs = import nixpkgs { inherit system; };
+    runtimeLibs = with pkgs; [
+      libGL libGLU libevent libffi libjpeg libpng libstartup_notification libvpx libwebp
+      stdenv.cc.cc fontconfig libxkbcommon zlib freetype
+      gtk3 libxml2 dbus xcb-util-cursor alsa-lib libpulseaudio pango atk cairo
+      gdk-pixbuf glib udev libva mesa libnotify cups pciutils
+      ffmpeg libglvnd pipewire
+    ] ++ (with pkgs.xorg; [
+      libxcb libX11 libXcursor libXrandr libXi libXext libXcomposite
+      libXdamage libXfixes libXScrnSaver
+    ]);
 
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      pname = "zen-browser";
+      version = zenSrc.zen.version;
 
-# generated.nix is created by nvfetcher and should provide `zenSrc` attrset
-zenSrc = import ./generated.nix;
+      src = pkgs.fetchurl {
+        inherit (zenSrc.zen.src) url sha256;
+      };
 
+      nativeBuildInputs = [ pkgs.makeWrapper pkgs.autoPatchelfHook ];
+      buildInputs = runtimeLibs;
 
-runtimeLibs = with pkgs; [
-libGL libGLU libevent libffi libjpeg libpng libstartup_notification libvpx libwebp
-stdenv.cc.cc fontconfig libxkbcommon zlib freetype
-gtk3 libxml2 dbus xcb-util-cursor alsa-lib libpulseaudio pango atk cairo
-gdk-pixbuf glib udev libva mesa libnotify cups pciutils
-ffmpeg libglvnd pipewire
-] ++ (with pkgs.xorg; [
-libxcb libX11 libXcursor libXrandr libXi libXext libXcomposite
-libXdamage libXfixes libXScrnSaver
-]);
+      installPhase = ''
+        mkdir -p $out/lib/zen
+        mkdir -p $out/bin
 
+        cp -r $src/* $out/lib/zen/
 
-mkZen = { variant }: pkgs.stdenv.mkDerivation {
-pname = "zen-browser";
-version = zenSrc.version;
+        makeWrapper $out/lib/zen/zen $out/bin/zen \
+          --set MOZ_LEGACY_PROFILES 1 \
+          --set MOZ_ALLOW_DOWNGRADE 1 \
+          --set MOZ_APP_LAUNCHER zen \
+          --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
 
+        install -D ./zen.desktop \
+          $out/share/applications/zen.desktop
 
-src = pkgs.fetchurl {
-inherit (zenSrc.src) url sha256;
-};
+        install -D $out/lib/zen/browser/chrome/icons/default/default128.png \
+          $out/share/icons/hicolor/128x128/apps/zen.png || true
+      '';
 
+      meta = with pkgs.lib; {
+        description = "Zen Browser Release (wrapped binary)";
+        homepage = "https://www.zen-browser.app/";
+        license = licenses.unfree;
+        platforms = [ "x86_64-linux" ];
+      };
+    };
 
-nativeBuildInputs = [ pkgs.makeWrapper pkgs.autoPatchelfHook pkgs.installShellFiles ];
+    defaultPackage = self.packages.${system}.default;
 
-
-buildInputs = runtimeLibs;
-
-
-installPhase = ''
-mkdir -p $out/lib/zen
-mkdir -p $out/bin
-
-
-cp -r $src/* $out/lib/zen/
-
-
-# main wrapper
-makeWrapper $out/lib/zen/zen $out/bin/zen \
---set MOZ_LEGACY_PROFILES 1 \
---set MOZ_ALLOW_DOWNGRADE 1 \
---set MOZ_APP_LAUNCHER zen \
---set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
-
-
-install -D ./zen.desktop $out/share/applications/zen.desktop
-install -D $out/lib/zen/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png || true
-
-
-# ensure exec permissions
-chmod -R a+x $out/lib/zen || true
-'';
-
-
-meta = with pkgs.lib; {
-description = "Zen Browser (unofficial, wrapped binary)";
-homepage = "https://www.zen-browser.app/";
-license = licenses.unfree; # binary distribution typically unfree; adjust if needed
-platforms = [ "x86_64-linux" ];
-};
-};
-
-
-in {
-packages.${system} = {
-specific = mkZen { variant = "specific"; };
-generic = mkZen { variant = "generic"; };
-default = self.packages.${system}.specific;
-};
-
-
-defaultPackage = self.packages.${system}.default;
-devShells.${system}.default = pkgs.mkShell { buildInputs = [ self.packages.${system}.default ]; };
-}
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ self.packages.${system}.default ];
+    };
+  }
 }
